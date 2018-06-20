d88391f63d40b351d532e0c063de9b11462797cf
daigz1224 <daigz1224@163.com>
2018-06-20 12:20:14 +0800

    update

--- boxplot_bris_all.py
+++ boxplot_bris_all.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 
 pkl_path = "/media/dai/ed9cf21d-a757-4514-b33a-34472199d3b2/daiguozheng_files/data_night_5_minutes.pkl"
+pkl_name = pkl_path.split('/')[-1]
 
 with open(pkl_path, 'rb') as f:
     bris_all = pickle.load(f)
@@ -19,5 +20,9 @@ for bris in bris_all:
     print('after filter 3 var: ' + str(len(bris_numpy_3var)))
     bris_all_3var.append(bris_numpy_3var.tolist())
 
+plt.figure()
 plt.boxplot(bris_all_3var)
+plt.xlabel("time index")
+plt.ylabel("brightness")
+plt.title(pkl_name)
 plt.show()
