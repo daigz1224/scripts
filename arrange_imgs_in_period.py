85fb73af2ee7a66177aebbef68ba14ff765d14b3
daigz1224 <daigz1224@163.com>
2018-06-20 09:48:52 +0800

    update arrange_imgs_in_period.py

--- arrange_imgs_in_period.py
+++ arrange_imgs_in_period.py
@@ -2,11 +2,11 @@ import os
 import shutil
 from matplotlib import pyplot as plt
 
-input_path = '/media/dai/ed9cf21d-a757-4514-b33a-34472199d3b2/daiguozheng_files/data_night'
+input_path = '/media/dai/ed9cf21d-a757-4514-b33a-34472199d3b2/daiguozheng_files/night_data/0606'
 input_name = input_path.split('/')[-1]
 
 period = 10  # 10 min
-output_dir = '/media/dai/ed9cf21d-a757-4514-b33a-34472199d3b2/daiguozheng_files' + '/data_night_' + str(period) + '_min'
+output_dir = '/media/dai/ed9cf21d-a757-4514-b33a-34472199d3b2/daiguozheng_files' + '/data_night_' + str(period) + '_minutes'
 
 if os.path.exists(output_dir) == 0:
     os.mkdir(output_dir)
@@ -17,16 +17,19 @@ def key_record(record):
     return int(num)
 
 recordlist = sorted(os.listdir(input_path), key=key_record)
-# because data_night have 2 source: 0606 and 0607
-sublist = [recordlist[i: i+period*2] for i in range(0, len(recordlist), period*2)]
+sublist = [recordlist[i: i+period] for i in range(0, len(recordlist), period)]
 print(str(len(sublist)) + ' sublist in total...')
 
 for index in range(len(sublist)):
+    ## 创建时间目录
     if os.path.exists(output_dir + '/' + str(index)) == 0:
         print('mkdir ' + output_dir + '/' + str(index) + '...')
         os.mkdir(output_dir + '/' + str(index))
+    ## 将每个record中的jpg拷贝到对应时间目录中
     for r in sublist[index]:
+        record_path = input_path + '/' + r            
         print('index = ' + str(index) + ', record = ' + r)
+        
         for f in os.listdir(input_path + '/' + r + '/image_data'):
             if f.endswith('.jpg'):
                 # print('copying ' + f + '...')
