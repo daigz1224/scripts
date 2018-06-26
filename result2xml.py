ba5352f8e963a53dca07f554aab7028fb2e029d4
daigz1224 <daigz1224@163.com>
2018-06-26 16:12:20 +0800

    update

--- result2xml.py
+++ result2xml.py
@@ -27,44 +27,44 @@ def result2xml(feilds):
     decodejson = json.loads(jsonstr)
     result_class = ImageResult(**decodejson)
 
-    
-        node_root = Element('annotation')
 
-        node_folder = SubElement(node_root, 'mark_task_511-20170515_ShangDi_ARZ034-2D_OBSTACLE_NO_GROUND_POINT')
-        node_folder.text = 'GTSDB'
+    node_root = Element('annotation')
 
-        node_filename = SubElement(node_root, 'filename')
-        node_filename.text = '000001.jpg'
+    node_folder = SubElement(node_root, 'mark_task_511-20170515_ShangDi_ARZ034-2D_OBSTACLE_NO_GROUND_POINT')
+    node_folder.text = 'GTSDB'
 
-        node_size = SubElement(node_root, 'size')
-        node_width = SubElement(node_size, 'width')
-        node_width.text = '500'
+    node_filename = SubElement(node_root, 'filename')
+    node_filename.text = '000001.jpg'
 
-        node_height = SubElement(node_size, 'height')
-        node_height.text = '375'
+    node_size = SubElement(node_root, 'size')
+    node_width = SubElement(node_size, 'width')
+    node_width.text = '500'
 
-        node_depth = SubElement(node_size, 'depth')
-        node_depth.text = '3'
+    node_height = SubElement(node_size, 'height')
+    node_height.text = '375'
 
-        node_object = SubElement(node_root, 'object')
-        node_name = SubElement(node_object, 'name')
-        node_name.text = 'mouse'
-        node_difficult = SubElement(node_object, 'difficult')
-        node_difficult.text = '0'
-        node_bndbox = SubElement(node_object, 'bndbox')
-        node_xmin = SubElement(node_bndbox, 'xmin')
-        node_xmin.text = '99'
-        node_ymin = SubElement(node_bndbox, 'ymin')
-        node_ymin.text = '358'
-        node_xmax = SubElement(node_bndbox, 'xmax')
-        node_xmax.text = '135'
-        node_ymax = SubElement(node_bndbox, 'ymax')
-        node_ymax.text = '375'
+    node_depth = SubElement(node_size, 'depth')
+    node_depth.text = '3'
 
-        xml = tostring(node_root, pretty_print=True)  # 格式化显示，该换行的换行
-        dom = parseString(xml)
-        print(xml)
-        return xml
+    node_object = SubElement(node_root, 'object')
+    node_name = SubElement(node_object, 'name')
+    node_name.text = 'mouse'
+    node_difficult = SubElement(node_object, 'difficult')
+    node_difficult.text = '0'
+    node_bndbox = SubElement(node_object, 'bndbox')
+    node_xmin = SubElement(node_bndbox, 'xmin')
+    node_xmin.text = '99'
+    node_ymin = SubElement(node_bndbox, 'ymin')
+    node_ymin.text = '358'
+    node_xmax = SubElement(node_bndbox, 'xmax')
+    node_xmax.text = '135'
+    node_ymax = SubElement(node_bndbox, 'ymax')
+    node_ymax.text = '375'
+
+    xml = tostring(node_root, pretty_print=True)  # 格式化显示，该换行的换行
+    dom = parseString(xml)
+    print(xml)
+    return xml
 
 
 def main(result_path, xml_dir):
